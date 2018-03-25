174096c4d2d54c7ec386337a3ec3964f10a2ae24
sun <sunfuchang03@126.com>
2018-03-25 20:23:11 +0800

    modify

--- src/main/java/com/belonk/jfinal/Config.java
+++ src/main/java/com/belonk/jfinal/Config.java
@@ -1,5 +1,7 @@
 package com.belonk.jfinal;
 
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.util.StringUtils;
 import com.belonk.jfinal.controller.HelloController;
 import com.belonk.jfinal.controller.IndexController;
 import com.belonk.jfinal.interceptor.ControlInjectlerInterceptor;
@@ -7,6 +9,7 @@ import com.belonk.jfinal.interceptor.GlobalActionInterceptor;
 import com.belonk.jfinal.interceptor.GlobalServiceInterceptor;
 import com.belonk.jfinal.model.Hello;
 import com.jfinal.config.*;
+import com.jfinal.kit.Prop;
 import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
@@ -34,7 +37,14 @@ public class Config extends JFinalConfig {
 
     @Override
     public void afterJFinalStart() {
+        System.out.println("After jfinal start ...");
+        super.afterJFinalStart();
+    }
 
+    @Override
+    public void beforeJFinalStop() {
+        System.out.println("Before jfinal stop ...");
+        super.beforeJFinalStop();
     }
 
     @Override
@@ -54,11 +64,25 @@ public class Config extends JFinalConfig {
 
     @Override
     public void configPlugin(Plugins me) {
+        Prop dbProp = PropKit.use("db.properties");
+        String db_host = dbProp.get("db_host").trim();
+
+        String db_host_port = dbProp.get("db_host_port");
+        db_host_port = !StringUtils.isEmpty(db_host_port) ? db_host_port.trim() : "3306";
+
+        String db_name = dbProp.get("db_name").trim();
+        String db_user = dbProp.get("db_user").trim();
+        String db_password = dbProp.get("db_password").trim();
+
+        String jdbc_url = "jdbc:mysql://" + db_host + ":" + db_host_port + "/" + db_name + "?" + "useUnicode=true&"
+                + "characterEncoding=utf8&" + "zeroDateTimeBehavior=convertToNull";
+
         // Druid插件
-        DruidPlugin dp = new DruidPlugin("jdbc:mysql://localhost/test", "root", "admin");
-        me.add(dp);
+        DruidPlugin druidPlugin = new DruidPlugin(jdbc_url, db_user, db_password);
+        druidPlugin.addFilter(new StatFilter());
+        me.add(druidPlugin);
         // active record插件
-        ActiveRecordPlugin arp = new ActiveRecordPlugin(dp);
+        ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
         arp.addMapping("hello", Hello.class);
         me.add(arp);
         arp.setDialect(new MysqlDialect());
